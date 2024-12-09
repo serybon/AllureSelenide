7517653783f8d5c6de0d83410c2bd7aaa9d328db
Siarhei B <bnds1988@gmail.com>
2024-12-09 19:50:51 +0300

    added Selenium grid config

--- src/test/java/pageobject/selenide/TestBase.java
+++ src/test/java/pageobject/selenide/TestBase.java
@@ -7,8 +7,10 @@ import com.codeborne.selenide.WebDriverRunner;
 //import com.epam.reportportal.service.ReportPortal;
 //import com.epam.reportportal.testng.ReportPortalTestNGListener;
 import org.openqa.selenium.OutputType;
+import org.openqa.selenium.Platform;
 import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.bidi.log.LogLevel;
+import org.openqa.selenium.remote.DesiredCapabilities;
 import org.testng.ITestResult;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
@@ -27,7 +29,19 @@ public class TestBase {
 
     @BeforeMethod
     public void methodSetup() {
-        Configuration.browser = System.getProperty("browser", "chrome");
+        String browser = System.getProperty("browser", "chrome");
+        String platform = System.getProperty("os", "windows");
+
+        DesiredCapabilities caps = new DesiredCapabilities();
+        caps.setBrowserName(browser);
+
+        switch (platform){
+            case "windows": caps.setPlatform(Platform.WINDOWS);
+            case "mac": caps.setPlatform(Platform.MAC);
+            case "linux": caps.setPlatform(Platform.LINUX);
+        }
+
+        Configuration.remote = "http://localhost:4444/wd/hub";
         Configuration.baseUrl = "https://litecart.stqa.ru/en/";
         Configuration.pageLoadTimeout = 5000;
         open(Configuration.baseUrl);
